24d1fe04b6a1e7692c8423ca35e6d9be2f972cdc
Swapnil Mishra <swapnil.mishra@signavio.com>
2020-05-22 03:54:31 +0200

    add loader for publish script

--- publish-to-gh-page.js
+++ publish-to-gh-page.js
@@ -1,10 +1,14 @@
 const ghpages = require("gh-pages")
+const ora = require('ora');
+
+const spinner = ora('Publishing blog').start();
 
 ghpages.publish('public',
     {
-        branch: 'gh-pages',
+        branch: 'master',
         repo: 'git@github.com:swapnilmishra/swapnilmishra.github.io.git'
     }
     , () => {
-        console.log(`Your blog is published 🚀`)
+        spinner.stopAndPersist({ symbol: `🚀` })
+        // console.log(`Your blog is published 🚀`)
     });
